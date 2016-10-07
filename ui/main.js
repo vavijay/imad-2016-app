84209d8db7180342f7bf2d69517af95e596d071b
vavijay <vavijayakumar@gmail.com>
2016-10-07 21:39:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,15 +2,17 @@ console.log('Loaded!');
 
 //Counter increment by button
 var objbutton = document.getElementById('btncounter');
+var updspan = document.getElementById('spancount');
 
 objbutton.onclick = function () {
     // create a request object
     var request = new XMLHttpRequest();
-    
+    updspan.innerHTML = "Request created";
     // capture the response and store in a variable
     request.onreadystatechange = function(){
+        updspan.innerHTML = "State Changed";
         if (request.readyState === XMLHttpRequest.DONE){
-            var updspan = document.getElementById('spancount');
+            
             if (request.status === 200){
                 var currcounter = request.responseText();
                
