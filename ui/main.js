f6866be3134fff6895fcc1fb8e6e0370d10a6bbb
vavijay <vavijayakumar@gmail.com>
2016-10-07 21:16:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
 console.log('Loaded!');
+
+//Counter increment by button
+var objbutton = document.getElementById('btncounter');
+
+button.onclick = function () {
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.Done){
+            if (request.status === 200){
+                var currcounter = request.responseText();
+                var updspan = document.getElementById('spancount');
+                updspan.innerHTML = currcounter.toString();
+            }
+            
+        }
+    };
+    
+    request.open('GET', 'http://vavijay.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
