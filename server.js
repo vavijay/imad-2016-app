2f793bdae4deea7e53f6bfb2f9f858fd98b456af
vavijay <vavijayakumar@gmail.com>
2016-10-07 17:31:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/MyArticle', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'myArticle.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
