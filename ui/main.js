b2f15521f1a960f193bae171fcc9aa129fbdf314
vavijay <vavijayakumar@gmail.com>
2016-10-07 21:51:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,12 @@ var updspan = document.getElementById('spancount');
 objbutton.onclick = function () {
     // create a request object
     var request = new XMLHttpRequest();
-    updspan.innerHTML = "Request created";
+    //updspan.innerHTML = "Request created";
     // capture the response and store in a variable
     request.onreadystatechange = function(){
         updspan.innerHTML = "State Changed";
         if (request.readyState === XMLHttpRequest.DONE){
-            updspan.innerHTML = "Request Done";
+            //updspan.innerHTML = "Request Done";
             //if (request.status === 200){
                 var currcounter = request.responseText();
                
